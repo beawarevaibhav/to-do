3b3ac83b889d583c1b641f7adc5fdfb1b4f5ca89
beawarevaibhav <vaibhavhzb8@gmai.com>
2025-10-23 20:49:50 +0530

    baisctodo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,49 +1,45 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 function App() {
-    const [todo, settodo] = useState("")
+    const [todo, settodo] = useState()
     const [todos, settodos] = useState([])
-
-
-    const handleedit =()=>{
-
-    }
-    const handledelete =()=>{
-        
-    }
-    const handleadd =()=>{
-        settodos([...todos, {todo,iscompleted:false}])
-        settodo("")
-     
+    const handleadd=()=>{
+        settodos([...todos, {todo, iscompleted:false}])
+        settodo('')
         
     }
     const handlechange =(e)=>{
         settodo(e.target.value)
+
     }
     return (
         <>
-        <div className="conatiner mx-auto my-5 rounded-2xl bg-violet-500">
-        <div className="addtodo">
-            <h1>add</h1>
-            <input type="text" value={todo} onChange={handlechange}  className='bg-white' />
-            <button onClick={handleadd} className='bg-red-300'>add todo</button>
-        </div>
-                <h1 className="bg-red-500 text-xl font-bold">my todo</h1>
-           <div className="todos">
-            {todos.map(items=>{
-            return <div className="todo flex">
-                <div className="text">{items.todo}</div>
-                <div className="button">
-                    <button onClick={handleedit} className='bg-violet-300 py-1 text-sm font-bold rounded-md mx-2'>edit</button>
-                    <button onClick={handledelete} className='bg-violet-300 py-1 text-sm font-bold rounded-md mx-2'>delete</button>
-                </div>
+        <div className="container w-full h-full mx-auto  ">
+            <h1 className='bg-violet-700 text-center '>To do app</h1>
+            <div className="todo flex justify-center items-center mt-4">
 
+                <input onChange={handlechange} value={todo} type="text" placeholder='Add a task' className='border-2 border-violet-700 rounded-md p-2'/>
+                <button onClick={handleadd} className='bg-violet-700 text-white p-2 rounded-md ml-2'>Add</button>
             </div>
-            })}
-           </div>
+            <div className="text flex  justify-between items-center mt-4 border-2 border-violet-700 rounded-md p-4  gap-4">
+                {todos.map(items=>{
+               return <div className="text">
+               
+                <h3>{items.todo}</h3>
+                <div className="button" >
+                    <button className=' bg-violet-500 text-white p-2 rounded-md mt-2 mx-4 my-4 flex gap-4'>Delete</button>
+                    <button className=' bg-violet-500 text-white p-2 rounded-md mt-2 mx-4 my-4 flex gap-4' >Add</button>
+                    
+                </div>
+                </div>
+                   } )}
+            
+            </div>
+            
+
         </div>
         
+         
         </>
     )
 }
